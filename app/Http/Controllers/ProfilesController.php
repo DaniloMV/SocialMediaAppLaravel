d12411c64f158de3f487881deedd47e47265c1c4
salman zafar <salmanzafar949@gmail.com>
2017-08-07 22:27:37 -0700

    first Commit to this website

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Session;
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    //
+
+    public function index($slug)
+    {
+         $user = User::where('slug', $slug)->first();
+        return view('profiles.profile') -> with('user', $user);
+    }
+
+    public function edit()
+    {
+        return view('profiles.edit')->with('info', Auth::user()->profile);
+    }
+
+    public function update(Request $r)
+    {
+          
+          $this->validate($r,[
+              'location' => 'required',
+              'about'    => 'required|max:255'
+          ]);
+         
+          Auth::user()->profile()->update([
+              'location' => $r->location,
+              'about' =>    $r->about
+          ]);
+
+         // dd(Auth::user()->profile);
+          session::flash('sucess', 'profile updated');
+          return redirect()->back();
+    }
+}
